8ef86975c312bbb34db49fd803294be25139c09e
bnray53 <bnray53@gmail.com>
2018-07-21 16:22:03 -0500

    Add mouseClick event

--- sketch.js
+++ sketch.js
@@ -1,53 +1,64 @@
-var resolution=20;
+var resolution = 20;
 var gridArray;
-var numOfObjects=0;
+var numOfObjects = 0;
 var gridXSize;
 var gridYSize;
 
 
 function setup() {
-  // put setup code here
-  createCanvas(800, 400);
-  background(100); 
-  gridXSize=width/resolution;
-  gridYSize=height/resolution; 
+    // put setup code here
+    createCanvas(800, 400);
+    background(100);
+    gridXSize = width / resolution;
+    gridYSize = height / resolution;
 
-  createGrid();
+    createGrid();
 }
 
 function draw() {
 
 }
 
-function createGrid(){
-     gridArray=[];
-        for(i=0;i<gridXSize;i++){
-            gridArray[i]=[];
-                for(j=0;j<gridYSize;j++){
-                    fillGrid(i,j);
-                }
+function createGrid() {
+    gridArray = [];
+    for (i = 0; i < gridXSize; i++) {
+        gridArray[i] = [];
+        for (j = 0; j < gridYSize; j++) {
+            fillGrid(i, j);
         }
-        console.log(numOfObjects);  
+    }
+    console.log(numOfObjects);
 }
 
-function fillGrid(x,y){
-    rect((x*resolution),(y*resolution),resolution,resolution);
+function fillGrid(x, y) {
+    rect((x * resolution), (y * resolution), resolution, resolution);
     var indicator;
-    var rand = floor(random(0,2));
-        if(rand==1){
-            fill("black");
-            indicator=1;
-        }else{
-             fill("white");
-             indicator=0;
-        }
-    gridArray[x][y]= new GridContent(indicator);  
+    var rand = floor(random(0, 2));
+    if (rand == 1) {
+        fill("black");
+        indicator = 1;
+    } else {
+        fill("white");
+        indicator = 0;
+    }
+    gridArray[x][y] = new GridContent(indicator);
     numOfObjects++;
     return;
 }
 
-class GridContent{
-    constructor(ind){
-        this.ind=ind;
+class GridContent {
+    constructor(ind) {
+        this.ind = ind;
     }
-}  
+}
+
+//Based on mouse-press event
+function mousePressed() {
+    //If the user is inside canvas  
+    if (mouseX < width && mouseY < height) {
+        console.log("Inside canvas area");
+    }else {
+    //If user is outside canvas  
+        console.log("Please click inside the canvas area");
+    }
+}
